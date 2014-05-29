3c9c61360a4bc5b75b699024d7cb4b687ba2299b
Joshua Ard <jard@widen.com>
2014-05-29 14:17:30 -0500

    Prompt script for user's name

---
+++ session-1/script.js
@@ -0,0 +1,10 @@
+//SCRIPTS
+
+window.onload = function() { 
+	var firstName = prompt("What is your first name?");
+	var lastName = prompt("What is your last name?");
+
+	// alert("Your name is " + firstName + " " + lastName);
+	document.write("Your name is " + firstName + " " + lastName);
+
+}
